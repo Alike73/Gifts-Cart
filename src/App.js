b310f3d5a8f90a9f803181b51c0f12b7b03fc798
Alike73 <alimzhanisla@gmail.com>
2022-12-14 18:53:33 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+  const [gifts, setGifts] = useState(data);
+  const removeGift = (id) => {
+    let newGifts = gifts.filter(gift => gift.id !== id);
+    setGifts(newGifts);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <h1>Total gifts {gifts.length} in cart</h1>
+      </div>
+      {gifts.map((element => {
+        const {id, gifty, image} = element;
+        return (
+          <div key={id}>
+            <div className="container">
+              <h2>{id} &#171;{gifty}&#187;</h2>
+            </div>
+            <div className="container">
+              <img src={image} alt="GiftImage" width="320px" />
+            </div>
+            <div className="container">
+              <button onClick={() => removeGift(id)}>remove</button>
+            </div>
+          </div>
+        )
+      }))}
+      <div className="container">
+        <button onClick={() => setGifts([])}>Clear list</button>
+      </div>
     </div>
   );
 }
